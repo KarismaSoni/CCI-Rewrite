52cff6c5cfb31b41a58503e5738b887386efafb4
KarismaSoni <karismavsoni93@gmail.com>
2015-12-03 18:27:24 -0600

    added custom styles, in index page  added some contents, js too

--- js/custom.js
+++ js/custom.js
@@ -6,9 +6,26 @@
 
 //Script for going from bottom to top
 
-    $('a.back-to-top').click(function() {
-    $('body').animate({
-        scrollTop: 0
-    }, '700');
-    return false;
+    $(document).ready(function() {
+        var offset = 220;
+        var duration = 500;
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > offset) {
+                $('.back-to-top').fadeIn(duration);
+            } else {
+                $('.back-to-top').fadeOut(duration);
+            }
+        });
+        
+        $('.back-to-top').click(function(event) {
+            event.preventDefault();
+            $('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
     });
+
+/*$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});*/
