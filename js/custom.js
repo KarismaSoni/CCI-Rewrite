75666ad17361b477bc33208da57b55996ecf1c77
bhadreshp05 <bhadreshp05@gmail.com>
2016-02-15 17:25:37 -0600

    color solutions page refactoring

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,33 @@
- // Script to Activate the Carousel
+// Script to activate the boostrap carousel
+$('.carousel').carousel({
+    interval: 4000 //changes the speed
+});
+
+// Script to active pgwSlider
+$(document).ready(function() {
+    $('.pgwSlideshow').pgwSlideshow({
+        autoSlide: true,
+        intervalDuration: 4000
+    });
+});
+
+// Script to scroll page to top
+$(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+        } else {
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
     
-    $('.carousel').carousel({
-        interval: 4000 //changes the speed
-    });
-
-//Script for going from bottom to top
-
-    $(document).ready(function() {
-        var offset = 220;
-        var duration = 500;
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > offset) {
-                $('.back-to-top').fadeIn(duration);
-            } else {
-                $('.back-to-top').fadeOut(duration);
-            }
-        });
-        
-        $('.back-to-top').click(function(event) {
-            event.preventDefault();
-            $('html, body').animate({scrollTop: 0}, duration);
-            return false;
-        })
-    });
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
 
 
