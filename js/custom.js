a2b7ae69437fdd92dad98bc2c5948714ab41212d
Patel, Bhadresh <bhadreshp05@gmail.com>
2016-03-16 20:27:19 -0500

    updated color tac page

--- js/custom.js
+++ js/custom.js
@@ -18,10 +18,17 @@
     });
 
 
-    // Script to active pgwSlider
-    $('.pgwSlideshow').pgwSlideshow({
+    // Script to active slider on color solution page
+    $('.cci-solutions-slider').pgwSlideshow({
         autoSlide: true,
         intervalDuration: 4000
+    });
+
+    // Script to active slider on ColorTec page
+    $('.cci-colorTac-slider').pgwSlideshow({
+        autoSlide: true,
+        intervalDuration: 4000,
+        displayList: false
     });
 
     // Script to scroll page to top
